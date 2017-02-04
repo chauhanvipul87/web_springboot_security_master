9df6afc5c7e331e340631e8cef37323bc02e0290
vipul Chauhan <test.jpatel1@gmail.com>
2017-02-04 18:11:00 +0530

    Inital Project commit

---
+++ SpringBootProject/src/main/java/eu/kielczewski/example/service/currentuser/CurrentUserDetailsService.java
@@ -0,0 +1,32 @@
+package eu.kielczewski.example.service.currentuser;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import eu.kielczewski.example.domain.CurrentUser;
+import eu.kielczewski.example.domain.User;
+import eu.kielczewski.example.service.user.UserService;
+
+@Service
+public class CurrentUserDetailsService implements UserDetailsService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrentUserDetailsService.class);
+    private final UserService userService;
+
+    @Autowired
+    public CurrentUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public CurrentUser loadUserByUsername(String email) throws UsernameNotFoundException {
+        LOGGER.debug("Authenticating user with email={}", email.replaceFirst("@.*", "@***"));
+        User user = userService.getUserByEmail(email).orElse(new User());
+        return new CurrentUser(user);
+    }
+
+}
